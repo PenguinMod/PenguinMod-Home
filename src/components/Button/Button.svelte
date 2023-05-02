0dcfebbfa6edb953423be0b58084be3168d39763
JeremyGamer13 <69337718+JeremyGamer13@users.noreply.github.com>
2023-05-01 23:13:40 -0600

    afgauygfuyawdtaygwfatwgtyd

--- src/components/Button/Button.svelte
+++ src/components/Button/Button.svelte
@@ -3,6 +3,10 @@
     export let link = false;
     export let label = "Button";
     export let noredirect = false;
+
+    // extras
+    export let icon = false;
+    export let color = false;
 </script>
 
 {#if link}
@@ -11,13 +15,25 @@
         target={noredirect ? "_blank" : "_self"}
         style="text-decoration: none;"
     >
-        <button class={highlighted ? "button button-highlight" : "button"}>
+        <button
+            class={(highlighted ? "button button-highlight" : "button") +
+                (color ? ` ${color}` : "")}
+        >
+            {#if icon}
+                <img src={`/${icon}`} alt={icon} style="margin-right:6px;" />
+            {/if}
             {@html label}
         </button>
     </a>
 {/if}
 {#if !link}
-    <button class={highlighted ? "button button-highlight" : "button"}>
+    <button
+        class={(highlighted ? "button button-highlight" : "button") +
+            (color ? ` ${color}` : "")}
+    >
+        {#if icon}
+            <img src={`/${icon}`} alt={icon} style="margin-right:6px;" />
+        {/if}
         {@html label}
     </button>
 {/if}
@@ -41,9 +57,25 @@
         flex-direction: row;
         align-items: center;
     }
+    .button:focus {
+        outline-width: 4px;
+    }
     .button-highlight {
         outline: 0px;
         background-color: white;
         color: #00c3ff;
+    }
+
+    .remix {
+        background-color: #48ac72;
+        outline-color: rgba(72, 172, 114, 0.35);
+    }
+    .gray {
+        background-color: #a1a1a1;
+        outline-color: rgba(155, 155, 155, 0.35);
+    }
+    .orange {
+        background-color: #ffab00;
+        outline-color: rgba(255, 171, 0, 0.35);
     }
 </style>
--- src/routes/upload/+page.svelte
+++ src/routes/upload/+page.svelte
@@ -17,8 +17,44 @@
 <div class="main">
     <NavigationMargin />
 
-    <div class="section section-info">
+    <div class="section-info">
         <h1>Upload</h1>
+    </div>
+
+    <div class="full">
+        <div class="card">
+            <div
+                style="display:flex;flex-direction:row;justify-content: space-between;"
+            >
+                <div style="width:50%;">
+                    <p class="important notmargin">Project Title</p>
+                    <input type="text" placeholder="My Project" />
+                    <p class="important notmargin" style="margin-top:24px">
+                        Instructions
+                    </p>
+                    <textarea
+                        placeholder="Tell others how to use or play your project..."
+                    />
+                    <p class="important notmargin">Notes and Credits</p>
+                    <textarea
+                        placeholder="List people that were apart of the creation of this project..."
+                    />
+                </div>
+                <div style="width:50%;">
+                    <img
+                        src="/empty-project.png"
+                        style="border-width:1px;border-style:solid;border-color:rgba(0, 0, 0, 0.1);width:100%;"
+                        alt="Project Thumbnail"
+                    />
+                    <button class="image-picker">Use my own image</button>
+                </div>
+            </div>
+            <div style="display:flex;flex-direction:row;margin-top:48px">
+                <Button label="Upload" icon="upload.svg" />
+                <Button label="Remix" color="remix" icon="remix.svg" />
+                <Button label="Update" color="orange" icon="update.svg" />
+            </div>
+        </div>
     </div>
 
     <div
@@ -31,19 +67,36 @@
             </i>
         </p>
     </div>
-
-    <div class="full">
-        <div class="card">
-            <p>Project Title</p>
-            <input type="text" placeholder="My Project" />
-            <Button label="Upload" />
-        </div>
-    </div>
 </div>
 
 <style>
     * {
         font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;
+    }
+
+    textarea {
+        width: 90%;
+        resize: none;
+        height: 100px;
+        border-radius: 6px;
+        border-style: dashed;
+        border-color: rgba(0, 162, 255, 0.15);
+        border-width: 2px;
+    }
+    textarea:focus {
+        border-color: rgba(0, 162, 255, 0.35);
+        outline: none;
+    }
+    input[type="text"] {
+        width: 90%;
+        border-radius: 6px;
+        border-color: rgba(0, 162, 255, 0.15);
+        border-width: 2px;
+        border-style: dashed;
+    }
+    input[type="text"]:focus {
+        border-color: rgba(0, 162, 255, 0.35);
+        outline: none;
     }
 
     .main {
@@ -54,9 +107,6 @@
         min-width: 1000px;
     }
 
-    .section {
-        margin: 8px;
-    }
     .section-info {
         background: #00c3ffad;
         height: 6rem;
@@ -83,5 +133,27 @@
         flex-direction: column;
         align-items: center;
         margin-top: 32px;
+    }
+
+    .important {
+        font-weight: bold;
+    }
+    .notmargin {
+        margin-block: 0;
+    }
+
+    .image-picker {
+        background-color: #00c3ff;
+        color: white;
+        border-radius: 1000px;
+        width: 100%;
+        border: 0px;
+        padding: 0.25rem 0;
+        outline: 0;
+        cursor: pointer;
+        font-weight: bold;
+    }
+    .image-picker:focus {
+        outline: 4px solid rgba(0, 195, 255, 0.3);
     }
 </style>
