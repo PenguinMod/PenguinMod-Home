aa72d85b8b65d7d99f092ede72dd7d3c1d83bf50
JeremyGamer13 <69337718+JeremyGamer13@users.noreply.github.com>
2023-05-07 11:15:30 -0600

    more redirects

---
+++ src/routes/PenguinMod-Packager/+page.svelte
@@ -0,0 +1,13 @@
+<script>
+    import { onMount } from "svelte";
+    onMount(() => {
+        window.location.href =
+            "https://studio.penguinmod.site/PenguinMod-Packager";
+    });
+</script>
+
+<head>
+    <title>Redirecting</title>
+</head>
+If you are not redirected automatically,
+<a href="https://studio.penguinmod.site/PenguinMod-Packager">click here</a>
