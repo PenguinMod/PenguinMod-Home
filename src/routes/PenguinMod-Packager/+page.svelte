cd8accc4d319de6fcd0b9a073d590bfa8924c792
JeremyGamer13 <69337718+JeremyGamer13@users.noreply.github.com>
2023-05-08 19:34:54 -0600

    style redirects

--- src/routes/PenguinMod-Packager/+page.svelte
+++ src/routes/PenguinMod-Packager/+page.svelte
@@ -9,5 +9,31 @@
 <head>
     <title>Redirecting</title>
 </head>
-If you are not redirected automatically,
-<a href="https://studio.penguinmod.site/PenguinMod-Packager">click here</a>
+<div>
+    If you are not redirected automatically,
+    <a href="https://studio.penguinmod.site/PenguinMod-Packager">click here</a>
+</div>
+
+<style>
+    * {
+        font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;
+        color: white;
+    }
+
+    a {
+        margin-left: 4px;
+    }
+
+    div {
+        background: #009ccc;
+        display: flex;
+        align-items: center;
+        height: 100%;
+        width: 100%;
+        position: absolute;
+        left: 0px;
+        top: 0px;
+        overflow: hidden;
+        justify-content: center;
+    }
+</style>
