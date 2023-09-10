20fd1272a3327d36871885d49e59d684f5aed385
JeremyGamer13 <69337718+JeremyGamer13@users.noreply.github.com>
2023-09-09 22:01:15 -0600

    yea

--- src/routes/sucessdonation/+page.svelte
+++ src/routes/sucessdonation/+page.svelte
@@ -10,15 +10,73 @@
     import LocalizedText from "$lib/LocalizedText/Node.svelte";
     import Language from "../../resources/language.js";
 
-    // Static values
-    import LINK from "../../resources/urls.js";
-
     let currentLang = "en";
     onMount(() => {
         Language.forceUpdate();
     });
     Language.onChange((lang) => {
         currentLang = lang;
+    });
+
+    function toRad(deg) {
+        return deg * (Math.PI / 180);
+    }
+
+    // TODO: rewrite ALL of this to be canvas :3
+    let penguinArea;
+    let penguinStyles = [];
+    const penguinData = [];
+    for (let i = 0; i < 10; i++) {
+        const moveDirection = Math.round(Math.random()) == 1 ? 1 : -1;
+        penguinStyles.push({
+            left: Math.round(Math.random() * 1280),
+            bottom: 0,
+            rotate: 0,
+            transform: `scaleX(${moveDirection})`,
+            "transform-origin": "center bottom",
+        });
+        penguinData.push({
+            rotation: Math.round(Math.random() * 360),
+            moving: moveDirection,
+        });
+    }
+    const psToStyle = (style) => {
+        let string = "";
+        for (const key in style) {
+            const unit =
+                typeof style[key] === "number" ? `${style[key]}px` : style[key];
+            if (key === "rotate") {
+                string += `${key}: ${style[key]}deg; `;
+                continue;
+            }
+            string += `${key}: ${unit}; `;
+        }
+        return string;
+    };
+    onMount(() => {
+        setInterval(() => {
+            let idx = 0;
+            for (const style of penguinStyles) {
+                style.left += penguinData[idx].moving * 2;
+                if (style.left > penguinArea.clientWidth - 80) {
+                    penguinData[idx].moving = -1;
+                    style.transform = "scaleX(-1)";
+                }
+                if (style.left < -80) {
+                    penguinData[idx].moving = 1;
+                    style.transform = "scaleX(1)";
+                }
+                penguinData[idx].rotation += 2;
+                penguinData[idx].rotation = penguinData[idx].rotation % 360;
+                style.bottom =
+                    Math.abs(Math.sin(toRad(penguinData[idx].rotation) * 6)) *
+                    15;
+                style.rotate =
+                    Math.sin(toRad(penguinData[idx].rotation) * 6) * 5;
+                idx++;
+            }
+            penguinStyles = penguinStyles;
+        }, 1000 / 15);
     });
 </script>
 
@@ -49,6 +107,17 @@
             We hope you continue to support PenguinMod, even if it's not with
             money but by sharing and using our service!
         </p>
+        <div class="bouncing-penguins" bind:this={penguinArea}>
+            {#each penguinStyles as style}
+                <img
+                    class="penguin"
+                    draggable="false"
+                    src="/penguins/cheer.svg"
+                    style={psToStyle(style)}
+                    alt="Penguin"
+                />
+            {/each}
+        </div>
         <Button link="/donate">Back</Button>
     </div>
 </div>
@@ -56,23 +125,23 @@
 <div class="confetti-holder">
     <div class="confetti-wrapper">
         <Confetti
-            amount="500"
-            size="50"
-            x={[-15, 15]}
-            y={[0, 50]}
-            noGravity
-            duration="10000"
-            fallDistance="100vh"
-        />
-        <Confetti
             amount="40"
             size="100"
             x={[-15, 15]}
             y={[0, 50]}
             noGravity
-            duration="10000"
+            duration="6000"
             fallDistance="100vh"
             colorArray={["url(/navicon.png)"]}
+        />
+        <Confetti
+            amount="200"
+            size="50"
+            x={[-15, 15]}
+            y={[0, 50]}
+            noGravity
+            duration="6000"
+            fallDistance="100vh"
         />
     </div>
 </div>
@@ -122,5 +191,20 @@
         left: 50%;
         bottom: 0;
         pointer-events: none;
+    }
+
+    .bouncing-penguins {
+        position: relative;
+        width: 100%;
+        height: 256px;
+        margin-bottom: 16px;
+        overflow-x: hidden;
+    }
+    .penguin {
+        position: absolute;
+        left: 0;
+        bottom: 0;
+        height: 100%;
+        user-select: none;
     }
 </style>
