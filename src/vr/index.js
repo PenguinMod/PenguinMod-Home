3f7b3939dfabc32e2738af0d33b73fc4b70f2da0
JeremyGamer13 <69337718+JeremyGamer13@users.noreply.github.com>
2023-11-08 21:49:49 -0700

    more testing stuff

--- src/vr/index.js
+++ src/vr/index.js
@@ -1,5 +1,5 @@
 import * as Three from "three";
-import { XRControllerModelFactory } from 'three/examples/jsm/webxr/XRControllerModelFactory';
+import { XRControllerModelFactory } from "three/examples/jsm/webxr/XRControllerModelFactory";
 // import BufferGeometryUtils from 'three/examples/jsm/utils/BufferGeometryUtils';
 // import { ConvexGeometry } from 'three/examples/jsm/geometries/ConvexGeometry';
 // import { OBJLoader } from 'three/examples/jsm/loaders/OBJLoader.js';
@@ -39,25 +39,66 @@ class VRHandler {
          * @type {XRSession}
          */
         this.session = null;
+
+        /**
+         * @type {Three.Group}
+         */
+        this.group = null;
+        /**
+         * @type {Three.Raycaster}
+         */
+        this.raycaster = null;
+        this.tempMatrix = new Three.Matrix4();
+
+        this.audioElements = {
+            background: new Audio("https://penguinmod.com/vr/wind.mp3"),
+
+            type: new Audio("https://penguinmod.com/vr/type.mp3"),
+            hover: new Audio("https://penguinmod.com/vr/hover.mp3"),
+            enter: new Audio("https://penguinmod.com/vr/enter.mp3"),
+        };
     }
 
     static isSupported() {
-        if (!('xr' in navigator)) return false;
+        if (!("xr" in navigator)) return false;
         return navigator.xr.isSessionSupported(SESSION_TYPE);
     }
-    
+
+    pauseAudioElements() {
+        for (const audioElement in this.audioElements) {
+            /**
+             * @type {HTMLAudioElement}
+             */
+            const audio = this.audioElements[audioElement];
+            if (audio) {
+                audio.pause();
+            }
+        }
+    }
+
     _disposeImmersive() {
         this.session = null;
+        this.hideVrCanvas();
+        this.pauseAudioElements();
         if (!this.renderer) return;
         this.renderer.xr.enabled = false;
-        this.hideVrCanvas();
     }
     async _createImmersive() {
         const renderer = this.renderer;
         if (!renderer) return false;
 
-        const sessionInit = { optionalFeatures: ['local-floor', 'bounded-floor', 'hand-tracking', 'layers'] };
-        const session = await navigator.xr.requestSession(SESSION_TYPE, sessionInit);
+        const sessionInit = {
+            optionalFeatures: [
+                "local-floor",
+                "bounded-floor",
+                "hand-tracking",
+                "layers",
+            ],
+        };
+        const session = await navigator.xr.requestSession(
+            SESSION_TYPE,
+            sessionInit
+        );
         this.session = session;
         this.isStarted = true;
 
@@ -83,13 +124,16 @@ class VRHandler {
 
             renderer.render(this.scene, this.camera);
             session.requestAnimationFrame(drawFrame);
-        }
+        };
         session.requestAnimationFrame(drawFrame);
 
         // reference space
-        session.requestReferenceSpace("local").then(space => {
+        session.requestReferenceSpace("local").then((space) => {
             this.localSpace = space;
         });
+
+        // play audio
+        this.playSessionAudio();
 
         return session;
     }
@@ -98,7 +142,7 @@ class VRHandler {
             const canvas = this.renderer.domElement;
             canvas.style.display = "";
         } catch {
-            console.warn('tried to show VR canvas');
+            console.warn("tried to show VR canvas");
         }
     }
     hideVrCanvas() {
@@ -106,15 +150,22 @@ class VRHandler {
             const canvas = this.renderer.domElement;
             canvas.style.display = "none";
         } catch {
-            console.warn('tried to hide VR canvas');
+            console.warn("tried to hide VR canvas");
         }
+    }
+
+    playSessionAudio() {
+        this.audioElements.background.loop = true;
+        this.audioElements.background.volume = 0.2;
+        this.audioElements.background.play();
     }
 
     initialize() {
         this.scene = new Three.Scene();
-        this.renderer = new Three.WebGLRenderer({ // TODO: is this appropriate config for our use-case?
+        this.renderer = new Three.WebGLRenderer({
+            // TODO: is this appropriate config for our use-case?
             preserveDrawingBuffer: true,
-            alpha: true
+            alpha: true,
         });
         this.renderer.setSize(1920, 1080); // TODO: is this too large or does it even matter?
         this.renderer.setClearColor(0x000000, 1);
@@ -123,52 +174,88 @@ class VRHandler {
         // skybox
         const cubeTexLoader = new Three.CubeTextureLoader();
         const skyboxTexture = cubeTexLoader.load([
-            'https://penguinmod.com/vr/skybox_right.png',
-            'https://penguinmod.com/vr/skybox_left.png',
-            'https://penguinmod.com/vr/skybox_top.png',
-            'https://penguinmod.com/vr/skybox_bottom.png',
-            'https://penguinmod.com/vr/skybox_front.png',
-            'https://penguinmod.com/vr/skybox_back.png',
+            "https://penguinmod.com/vr/skybox_right.png",
+            "https://penguinmod.com/vr/skybox_left.png",
+            "https://penguinmod.com/vr/skybox_top.png",
+            "https://penguinmod.com/vr/skybox_bottom.png",
+            "https://penguinmod.com/vr/skybox_front.png",
+            "https://penguinmod.com/vr/skybox_back.png",
         ]);
         this.scene.background = skyboxTexture;
+
+        this.group = new Three.Group();
+        this.scene.add(this.group);
+
         // platform
         const texLoader = new Three.TextureLoader();
-        const platformTexture = texLoader.load('https://penguinmod.com/vr/platform.png');
+        const platformTexture = texLoader.load(
+            "https://penguinmod.com/vr/platform.png"
+        );
         const geometry = new Three.PlaneGeometry(1, 1);
         const material = new Three.MeshBasicMaterial({
             map: platformTexture,
-            side: Three.DoubleSide
+            side: Three.DoubleSide,
         });
         const platformObject = new Three.Mesh(geometry, material);
         platformObject.position.set(0, 0, 0);
         platformObject.rotateX(toRad(90));
         this.scene.add(platformObject);
-		// controllers
-		const controller1 = this.renderer.xr.getController(0);
-		// controller1.addEventListener( 'selectstart', onSelectStart );
-		// controller1.addEventListener( 'selectend', onSelectEnd );
-		this.scene.add(controller1);
 
-		const controller2 = this.renderer.xr.getController(1);
-		// controller2.addEventListener( 'selectstart', onSelectStart );
-		// controller2.addEventListener( 'selectend', onSelectEnd );
-		this.scene.add(controller2);
+        // controllers
+        const controller1 = this.renderer.xr.getController(0);
+        controller1.addEventListener('selectstart', this.onSelectStart.bind(this));
+        controller1.addEventListener('selectend', this.onSelectEnd.bind(this));
+        this.scene.add(controller1);
 
-		const controllerModelFactory = new XRControllerModelFactory();
+        const controller2 = this.renderer.xr.getController(1);
+        controller2.addEventListener('selectstart', this.onSelectStart.bind(this));
+        controller2.addEventListener('selectend', this.onSelectEnd.bind(this));
+        this.scene.add(controller2);
 
-		const controllerGrip1 = this.renderer.xr.getControllerGrip(0);
-		controllerGrip1.add(controllerModelFactory.createControllerModel(controllerGrip1));
-		this.scene.add(controllerGrip1);
+        const controllerModelFactory = new XRControllerModelFactory();
 
-		const controllerGrip2 = this.renderer.xr.getControllerGrip(1);
-		controllerGrip2.add(controllerModelFactory.createControllerModel(controllerGrip2));
-		this.scene.add(controllerGrip2);
+        const controllerGrip1 = this.renderer.xr.getControllerGrip(0);
+        controllerGrip1.add(
+            controllerModelFactory.createControllerModel(controllerGrip1)
+        );
+        this.scene.add(controllerGrip1);
+
+        const controllerGrip2 = this.renderer.xr.getControllerGrip(1);
+        controllerGrip2.add(
+            controllerModelFactory.createControllerModel(controllerGrip2)
+        );
+        this.scene.add(controllerGrip2);
 
         // light
         const light = new Three.SpotLight(0xffffff, 60);
-        light.position.set(0, 5, 2.5)
+        light.position.set(0, 5, 2.5);
         this.scene.add(light);
+
+        // line
+        const lineGeometry = new Three.BufferGeometry().setFromPoints([
+            new Three.Vector3(0, 0, 0),
+            new Three.Vector3(0, 0, -1),
+        ]);
+
+        const line = new Three.Line(lineGeometry);
+        line.name = "line";
+        line.scale.z = 5;
+
+        controller1.add(line.clone());
+        controller2.add(line.clone());
+
+        this.raycaster = new Three.Raycaster();
+
+        // test
+        const testCubeGeometry = new Three.PlaneGeometry(1, 1);
+        const testCubeMaterial = new Three.MeshBasicMaterial({
+            color: 0xff0000
+        });
+        const testCubeObject = new Three.Mesh(testCubeGeometry, testCubeMaterial);
+        testCubeObject.position.set(0, 4, -5);
+        this.group.add(testCubeObject);
     }
+
     start() {
         if (this.isStarted) return;
         if (this.session) return;
@@ -176,9 +263,43 @@ class VRHandler {
     }
     close() {
         this.isStarted = false;
+        this.hideVrCanvas();
+        this.pauseAudioElements();
         if (!this.session) return;
         return this.session.end();
     }
+
+    onSelectStart(event) {
+        const controller = event.target;
+        const intersections = this.getIntersections(controller);
+        if (intersections.length > 0) {
+            const intersection = intersections[0];
+
+            const object = intersection.object;
+            // object.material.emissive.b = 1;
+            controller.attach(object);
+
+            controller.userData.selected = object;
+        }
+        controller.userData.targetRayMode = event.data.targetRayMode;
+    }
+    onSelectEnd(event) {
+        const controller = event.target;
+
+        if (controller.userData.selected !== undefined) {
+            const object = controller.userData.selected;
+            this.group.attach(object);
+
+            controller.userData.selected = undefined;
+        }
+    }
+    getIntersections(controller) {
+		controller.updateMatrixWorld();
+		this.tempMatrix.identity().extractRotation(controller.matrixWorld);
+		this.raycaster.ray.origin.setFromMatrixPosition(controller.matrixWorld);
+		this.raycaster.ray.direction.set(0, 0, -1).applyMatrix4(this.tempMatrix);
+		return this.raycaster.intersectObjects(this.group.children, false);
+	}
 }
 
 export default VRHandler;
