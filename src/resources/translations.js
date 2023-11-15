bc9ee045734f8b5fe78469caaa5b0713b6ae809e
godslayerakp <74981904+RedMan13@users.noreply.github.com>
2023-11-15 01:43:53 -0800

    Update translations.js

--- src/resources/translations.js
+++ src/resources/translations.js
@@ -6,6 +6,7 @@
 import en from '../translations/en.json';
 import id from '../translations/id.json';
 import he from '../translations/he.json';
+import uwu from '../translations/uwu.json';
 import test from '../translations/test.json';
 
 // to add a language, the first part is the language code (ex: "es-419") or top level (ex: "en-US" goes to "en")
@@ -15,7 +16,7 @@ const languages = {
     "en": en,
     "id": id,
     "he": he,
-
+    "uwu": uwu
     // "test": test,
 };
 // add languages that are RTL here
