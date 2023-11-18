905c3356d3667c4becc6924766bcec4f2aa9c26a
Steve0Greatness <75220768+Steve0Greatness@users.noreply.github.com>
2023-11-18 13:55:37 -0800

    Removed CSS about Donations.

--- src/routes/auth/+page.svelte
+++ src/routes/auth/+page.svelte
@@ -28,7 +28,7 @@
 
     var ProjectCommentAuthOpen = false;
     var ProfileCommentAuthOpen = false;
-    
+
     var AuthCode = "";
     var AuthUser = "";
     
@@ -189,69 +189,6 @@
     }
     .section-details {
         width: 30%;
-    }
-
-    .donation-container {
-        position: relative;
-        border-radius: 6px;
-        border: 1px solid rgba(0, 0, 0, 0.5);
-        background: white;
-        overflow: hidden;
-        padding-left: 60px;
-        display: flex;
-        flex-direction: row;
-        align-items: center;
-        cursor: pointer;
-        box-shadow: 1px 1px 2px black;
-        height: 52px;
-    }
-    .donation-container:hover {
-        box-shadow: 1px 1px 10px black;
-    }
-    .donation-container:active {
-        background: rgb(219, 219, 219);
-    }
-    .donation-container > img {
-        position: absolute;
-        left: 0;
-        top: 0;
-        height: calc(100% + 1px);
-    }
-    .donation-container > span {
-        font-size: 20px;
-        margin-right: 8px;
-        font-weight: bold;
-        text-decoration: none !important;
-    }
-
-    :global(body.dark-mode) .donation-container {
-        background: transparent;
-        border-color: white;
-        color: white;
-        box-shadow: 1px 1px 2px white;
-    }
-    :global(body.dark-mode) .donation-container:hover {
-        box-shadow: 1px 1px 10px white;
-    }
-    :global(body.dark-mode) .donation-container:active {
-        background: rgba(255, 255, 255, 0.2);
-    }
-
-    .donation-section {
-        display: flex;
-        flex-direction: row;
-    }
-    .donation-buttons {
-        width: 50%;
-    }
-    .donation-images {
-        width: 50%;
-        display: flex;
-        flex-direction: row;
-        justify-content: flex-end;
-    }
-    .donation-images img {
-        height: 192px;
     }
 
     :global(body.dark-mode) a {
