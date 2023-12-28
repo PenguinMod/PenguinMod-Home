81e6b8eb09e5d00e48823aea522a744b19f6f3b7
JeremyGamer13 <69337718+JeremyGamer13@users.noreply.github.com>
2023-12-27 17:23:12 -0700

    add 6 languages & do small things

--- src/translations/!locales.js
+++ src/translations/!locales.js
@@ -15,8 +15,14 @@ import ro from '../translations/ro.json';
 import sv from '../translations/sv.json';
 import ru from '../translations/ru.json';
 import cs from '../translations/cs.json';
+import de from '../translations/de.json';
 import hr from '../translations/hr.json';
 import vi from '../translations/vi.json';
+import frCa from '../translations/fr-ca.json';
+import fr from '../translations/fr.json';
+import no from '../translations/no.json';
+import sk from '../translations/sk.json';
+import uk from '../translations/uk.json';
 import uwu from '../translations/uwu.json';
 import test from '../translations/test.json';
 
@@ -32,10 +38,16 @@ const languages = {
     "vi": vi,                      // Viatnamese
     "ro": ro,                      // Romanian
     "ru": ru,                      // Russian
+    "fr": fr,                      // French
+    "fr-ca": frCa,                 // French (Canada)
+    "de": de,                      // German
     "da": da,                      // Danish
     "pl": pl,                      // Polish
+    "no": no,                      // Norwegian
     "tr": tr,                      // Turkish
     "sv": sv,                      // Swedish
+    "sk": sk,                      // Slovak
+    "uk": uk,                      // Ukranian
     "id": id,                      // Bahasa Indonesia
     "he": he,                      // Hebrew
     // uncomment joke languages when we have >30 regular langs to hide them
