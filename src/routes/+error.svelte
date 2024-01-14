24372bdd3d0e4d2c85b245a0b0890b80564fdbdb
godslayerakp <74981904+RedMan13@users.noreply.github.com>
2024-01-13 16:32:42 -0800

    oh woops

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -113,7 +113,7 @@
             </button>
         {:else}
             <button class="game" on:click={gameClick}>
-                <p class="game-points" on:click{gameClick}>{gameState.points.toLocaleString()}</p>
+                <p class="game-points" on:keydown{gameClick}>{gameState.points.toLocaleString()}</p>
                 <img
                     src="/secret/pengin.svg"
                     alt="Penguin"
