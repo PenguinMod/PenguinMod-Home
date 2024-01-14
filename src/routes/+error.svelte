01c3aa2cb08e44a186dc698d26ae0b8e8df3476e
JeremyGamer13 <69337718+JeremyGamer13@users.noreply.github.com>
2024-01-13 17:11:25 -0700

    add thing

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -7,6 +7,88 @@
 
     // Icons
     import PenguinConfusedSVG from "../icons/Penguin/confused.svelte";
+
+    let gameIsActive = false;
+    const gameAssets = {
+        penguin: null,
+        brick: null
+    };
+    const gameState = {
+        playerY: 0,
+        playerFalling: 0,
+        brickX: 500,
+        brickY: -200,
+        grassX: 0,
+        points: 0,
+        canIncreasePoints: true,
+    };
+    const defaultGameState = JSON.parse(JSON.stringify(gameState));
+    const gameClick = () => {
+        gameState.playerFalling = -5;
+    };
+    const resetState = () => {
+        for (const key of Object.keys(defaultGameState)) {
+            gameState[key] = defaultGameState[key];
+        }
+    };
+    const rectTouch = (rect1, rect2) => {
+        if (rect1.x + rect1.width < rect2.x || rect2.x + rect2.width < rect1.x) {
+            return false;
+        }
+        if (rect1.y + rect1.height < rect2.y || rect2.y + rect2.height < rect1.y) {
+            return false;
+        }
+        return true;
+    };
+    const startGame = () => {
+        gameIsActive = true;
+        setInterval(() => {
+            gameState.playerY += gameState.playerFalling;
+            if (gameState.playerFalling < 20) {
+                gameState.playerFalling += 0.25;
+            }
+            if (gameState.playerY > 220) {
+                resetState();
+            }
+            if (gameState.playerY < 0) {
+                gameState.playerY = 0;
+            }
+            gameState.grassX -= 3;
+            gameState.grassX = gameState.grassX % 400;
+            gameState.brickX -= 3;
+            // collide
+            const playerRect = {
+                x: 40,
+                y: gameState.playerY,
+                width: 33.55,
+                height: 25.98
+            };
+            const brick1Rect = {
+                x: gameState.brickX,
+                y: gameState.brickY,
+                width: 55,
+                height: 300
+            };
+            const brick2Rect = {
+                ...brick1Rect,
+                y: brick1Rect.y + 400
+            };
+            const willReset = rectTouch(playerRect, brick1Rect) || rectTouch(playerRect, brick2Rect);
+            if (willReset) {
+                resetState();
+            }
+            if (gameState.brickX + 25 < 40 && gameState.canIncreasePoints) {
+                gameState.points += 1;
+                gameState.canIncreasePoints = false;
+            }
+            // reset brick if needed
+            if (gameState.brickX < -100) {
+                gameState.brickX = defaultGameState.brickX;
+                gameState.brickY = -160 - (Math.random() * 120); // -160 to -280
+                gameState.canIncreasePoints = true;
+            }
+        }, 1000 / 60);
+    };
 </script>
 
 <svelte:head>
@@ -24,7 +106,38 @@
 <NavigationMargin />
 
 <div class="center-div">
-    <PenguinConfusedSVG height="12rem" />
+    {#if $page.status === 404}
+        {#if !gameIsActive}
+            <button class="hidden-button" on:click={startGame}>
+                <PenguinConfusedSVG height="12rem" />
+            </button>
+        {:else}
+            <button class="game" on:click={gameClick}>
+                <p class="game-points">{gameState.points.toLocaleString()}</p>
+                <img
+                    src="/secret/pengin.svg"
+                    alt="Penguin"
+                    class="game-penguin"
+                    style="top: {gameState.playerY}px"
+                    bind:this={gameAssets.penguin}
+                >
+                <img
+                    src="/secret/floor.png"
+                    alt="Grass"
+                    style="left: {gameState.grassX}px"
+                    class="game-floor"
+                >
+                <img
+                    src="/secret/bricks.png"
+                    alt="Brick"
+                    style="left: {gameState.brickX}px; top: {gameState.brickY}px;"
+                    class="game-bricks"
+                >
+            </button>
+        {/if}
+    {:else}
+        <PenguinConfusedSVG height="12rem" />
+    {/if}
 
     <h1>Whoops!</h1>
     <p>
@@ -37,6 +150,54 @@
 <style>
     * {
         font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;
+    }
+
+    .game {
+        width: 400px;
+        height: 300px;
+        margin: 8px;
+        padding: 0;
+        border: 0;
+        position: relative;
+        background-image: url('/secret/sky.png');
+        overflow: hidden;
+    }
+    .game-penguin {
+        position: absolute;
+        left: 40px;
+        top: 0;
+        transform: scale(0.5);
+        transform-origin: left top;
+        z-index: 9990;
+    }
+    .game-floor {
+        position: absolute;
+        left: 0;
+        bottom: -20px;
+        z-index: 9998;
+    }
+    .game-bricks {
+        position: absolute;
+        z-index: 9997;
+    }
+    .game-points {
+        pointer-events: none;
+        font-size: 16px;
+        font-weight: bold;
+        font-style: italic;
+        color: black;
+        position: absolute;
+        left: 8px;
+        top: 8px;
+        margin-block: 0;
+        z-index: 9999;
+    }
+
+    .hidden-button {
+        padding: 0;
+        margin: 0;
+        border: 0;
+        background: transparent;
     }
 
     .center-div {
