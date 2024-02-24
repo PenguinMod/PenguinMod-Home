19b61f2fc031e78d0be61d8be9e119636ae5c6cd
JeremyGamer13 <69337718+JeremyGamer13@users.noreply.github.com>
2024-02-24 01:37:26 -0700

    add embed to post username info to PM

---
+++ src/routes/embed/editor/+page.svelte
@@ -0,0 +1,67 @@
+<script>
+    import { onMount } from "svelte";
+    import Authentication from "../../../resources/authentication.js";
+
+    const getLoginDetails = async () => {
+        const privateCode = localStorage.getItem("PV");
+        if (!privateCode) {
+            return { loggedIn: false };
+        }
+
+        try {
+            const { username } = await Authentication.usernameFromCode(privateCode);
+            if (!username) return { loggedIn: false };
+            return {
+                loggedIn: true,
+                username
+            };
+        } catch (e) {
+            console.warn(e);
+            return { loggedIn: false };
+        }
+    };
+
+    onMount(async () => {
+        const params = new URLSearchParams(location.search);
+        const importLocation = params.get("external");
+        if (!importLocation) return console.warn("No external provided");
+
+        const urlObject = new URL(importLocation);
+        if (
+            !(
+                urlObject.host.startsWith("dev.penguinmod.com") ||
+                urlObject.host.startsWith("studio.penguinmod.com") ||
+                urlObject.host.startsWith("penguinmod.com") ||
+                urlObject.host.startsWith("localhost:3000")
+            )
+        ) {
+            console.warn("Unsafe host", urlObject.host);
+            return;
+        }
+
+        // get parent window
+        const opener = window.opener || window.parent;
+        if (!opener || opener === window) {
+            // exit if not found
+            console.warn("No parents");
+            return;
+        }
+
+        // wrapper to handle errors & be easier
+        function post(type, data) {
+            try {
+                opener.postMessage(
+                    {
+                        type,
+                        packet: data,
+                    },
+                    importLocation
+                );
+            } catch (e) {
+                console.warn("Cannot post message", e);
+            }
+        }
+
+        post("login", await getLoginDetails());
+    });
+</script>
