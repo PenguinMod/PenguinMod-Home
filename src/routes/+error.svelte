7113f049d1b406f977b7a5b7cff34597bc56fa76
JeremyGamer13 <69337718+JeremyGamer13@users.noreply.github.com>
2024-03-24 19:27:08 -0600

    new error page & resize images that are too big

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -1,156 +1,73 @@
 <script>
     import { page } from "$app/stores";
+    import { onMount } from "svelte";
 
     // Components
     import NavigationBar from "$lib/NavigationBar/NavigationBar.svelte";
     import NavigationMargin from "$lib/NavigationBar/NavMargin.svelte";
+    import Button from "$lib/Button/Button.svelte";
+    import Error404 from "$lib/Game404/Component.svelte";
+    
+    import LocalizedText from "$lib/LocalizedText/Node.svelte";
+    import TranslationHandler from "../resources/translations.js";
+    import Language from "../resources/language.js";
 
-    // localstorage
-    import { browser } from '$app/environment';
+    let currentLang = "en";
+    onMount(() => {
+        Language.forceUpdate();
+    });
+    Language.onChange((lang) => {
+        currentLang = lang;
+    });
 
-    // Icons
-    import PenguinConfusedSVG from "../icons/Penguin/confused.svelte";
+    let pageStatus = Number($page.url.searchParams.get("error")) || $page.status;
 
-    let gameIsActive = false;
-    let currentMenu = 'start';
-    const gameAssets = {
-        penguin: null,
-        brick: null
+    let displayGame = false;
+    let showGameVignette = false;
+    const showGame = () => {
+        if (pageStatus !== 404) return;
+        displayGame = true;
+        showGameVignette = true;
     };
-    const constants = {
-        floorHeight: 240,
-        jumpHeight: 4
+    const hideGame = () => {
+        showGameVignette = false;
     };
-    const gameState = {
-        playerY: 150,
-        playerFalling: 0,
-        brickX: 500,
-        brickY: -200,
-        grassX: 0,
-        points: 0,
-        rotation: 0,
-        mustBeTop: false,
-        canIncreasePoints: true,
-    };
-    let highscore = browser ? (localStorage.getItem("flappypangHighscore") || 0) : 0;
-    const defaultGameState = {...gameState};
-    let gameButton = null;
-    let buttonUninitialized = true;
-    const gameClick = () => {
-        gameState.playerFalling = -constants.jumpHeight;
-    };
-    const resetState = () => {
-        for (const key of Object.keys(defaultGameState)) {
-            gameState[key] = defaultGameState[key];
+
+    const getErrorImage = (status) => {
+        switch (status) {
+            case 401:
+            case 403:
+            case 405:
+            case 408:
+            case 409:
+            case 413:
+            case 415:
+            case 429:
+            case 501:
+            case 502:
+            case 503:
+            case 504:
+            case 507:
+            case 508:
+                return `/errors/${status}.png`;
+            case 400:
+                return "/vr/warning.png";
+            case 500:
+                return "/penguins/server.svg";
+            default:
+                return "/penguins/confused.svg";
         }
     };
-    const rectTouch = (rect1, rect2) => {
-        if (rect1.x + rect1.width < rect2.x || rect2.x + rect2.width < rect1.x) {
-            return false;
-        }
-        if (rect1.y + rect1.height < rect2.y || rect2.y + rect2.height < rect1.y) {
-            return false;
-        }
-        return true;
-    };
-    const gameStartPressed = () => {
-        buttonUninitialized = true;
-        currentMenu = 'game';
+    const getTranslatedError = (status, currentLang) => {
+        return TranslationHandler.text(`navigation.error.${status}`, currentLang)
+            || TranslationHandler.text(`navigation.error.${status}`, 'en')
+            || TranslationHandler.text(`navigation.error.unknown`, currentLang)
+            || TranslationHandler.text(`navigation.error.unknown`, 'en');
     }
-    const restartGame = () => {
-        resetState();
-        gameStartPressed();
-    }
-    const die = () => {
-        gameState.mustBeTop = true;
-        currentMenu = 'died';
-        gameState.playerFalling = -constants.jumpHeight;
-        // just incase, push the sprite out of the ground
-        gameState.playerY += gameState.playerFalling;
-    }
-    const openDeathMenu = () => {
-        currentMenu = 'death'
-    }
-    const startGame = () => {
-        gameIsActive = true;
-        setInterval(() => {
-            switch (currentMenu) {
-            case 'start':
-                gameState.playerY = (Math.sin(Date.now() / 100) * 10) + 100
-                break;
-            case 'died':
-                gameState.playerY += gameState.playerFalling;
-                // gravity
-                if (gameState.playerFalling < 20) {
-                    gameState.playerFalling += 0.25;
-                }
-                if (gameState.playerY > 300) openDeathMenu();
-                break;
-            case 'game':
-                if (buttonUninitialized) {
-                    gameButton.focus();
-                    buttonUninitialized = false;
-                }
-                gameState.playerY += gameState.playerFalling;
-                // gravity
-                if (gameState.playerFalling < 20) {
-                    gameState.playerFalling += 0.25;
-                }
-                // death
-                if (gameState.playerY > constants.floorHeight) {
-                    die();
-                }
-                if (gameState.playerY < 0) {
-                    gameState.playerY = 0;
-                }
-                gameState.grassX -= 3;
-                gameState.grassX = gameState.grassX % 400;
-                gameState.brickX -= 3;
-                // collide
-                const playerRect = {
-                    x: 40,
-                    y: gameState.playerY,
-                    width: 33.55,
-                    height: 25.98
-                };
-                const brick1Rect = {
-                    x: gameState.brickX,
-                    y: gameState.brickY,
-                    width: 55,
-                    height: 300
-                };
-                const brick2Rect = {
-                    ...brick1Rect,
-                    y: brick1Rect.y + 400
-                };
-                const willReset = rectTouch(playerRect, brick1Rect) || rectTouch(playerRect, brick2Rect);
-                if (willReset) {
-                    die();
-                }
-                if (gameState.brickX + 25 < 40 && gameState.canIncreasePoints) {
-                    gameState.points += 1;
-                    gameState.canIncreasePoints = false;
-                    if (gameState.points > highscore) {
-                        highscore = gameState.points;
-                        if (browser) {
-                            localStorage.setItem("flappypangHighscore", highscore);
-                        }
-                    }
-                }
-                // reset brick if needed
-                if (gameState.brickX < -100) {
-                    gameState.brickX = defaultGameState.brickX;
-                    gameState.brickY = -160 - (Math.random() * 120); // -160 to -280
-                    gameState.canIncreasePoints = true;
-                }
-                break;
-            }
-        }, 1000 / 60);
-    };
 </script>
 
 <svelte:head>
-    <title>PenguinMod - {$page.status}</title>
+    <title>PenguinMod - {pageStatus}</title>
     <meta name="title" content="PenguinMod - Home" />
     <meta property="og:title" content="PenguinMod - Home" />
     <meta property="twitter:title" content="PenguinMod - Home">
@@ -164,141 +81,147 @@
 <NavigationMargin />
 
 <div class="center-div">
-    {#if $page.status === 404}
-        {#if !gameIsActive}
-            <button class="hidden-button" on:click={startGame} on:keydown={gameClick}>
-                <PenguinConfusedSVG height="12rem" />
-            </button>
-        {:else}
-            <button class="game" on:click={gameClick} bind:this={gameButton}>
-                {#if currentMenu === 'start'}
-                    <button class="menuButton" on:click={gameStartPressed}>start game</button>
-                {:else if currentMenu === 'death'}
-                    <img
-                        src="/secret/penginDead.svg"
-                        alt="Penguin Deid"
-                        class="menu-penguin"
-                        style="margin-bottom: 10px;position: relative;z-index: {gameState.mustBeTop ? 9999 : 9990}"
-                        bind:this={gameAssets.penguin}
-                    >
-                    <br/>
-                    <button class="menuButton" on:click={restartGame}>restart game</button>
-                {:else if currentMenu === 'game'}
-                    <p class="game-points score-counter">{gameState.points.toLocaleString()}</p>
-                    <p class="high-score score-counter">highscore<br>{highscore.toLocaleString()}</p>
-                {/if}
-                <img
-                    src="/secret/floor.png"
-                    alt="Grass"
-                    style="left: {gameState.grassX}px"
-                    class="game-floor"
-                >
-                <img
-                    src="/secret/bricks.png"
-                    alt="Brick"
-                    style="left: {gameState.brickX}px; top: {gameState.brickY}px;"
-                    class="game-bricks"
-                >
-                <img
-                    src="/secret/pengin{currentMenu === 'died' ? 'Dead' : ''}.svg"
-                    alt="Penguin"
-                    class="game-penguin"
-                    style="top: {gameState.playerY}px; z-index: {gameState.mustBeTop ? 9999 : 9990}"
-                    bind:this={gameAssets.penguin}
-                >
-            </button>
-        {/if}
-    {:else}
-        <PenguinConfusedSVG height="12rem" />
-    {/if}
+    <div class="card">
+        <div class="text-container">
+            <h1>{pageStatus}</h1>
+            <h2>{getTranslatedError(pageStatus, currentLang)}</h2>
+        </div>
 
-    <h1>Whoops!</h1>
-    <p>
-        Something's not quite right. Maybe you should <a href="../">go back?</a>
-    </p>
+        <div class="image-container">
+            <button class="image-button" on:click={showGame}>
+                <img
+                    src={getErrorImage(pageStatus)}
+                    alt="{pageStatus} - {$page.error.message}"
+                    title="{pageStatus} - {$page.error.message}"
+                />
+            </button>
+        </div>
 
-    <p>Error: {$page.status} - {$page.error.message}</p>
+        <a href="../" class="bottom-left">
+            <Button>
+                <LocalizedText
+                    text="Go back"
+                    key="redirect.leave"
+                    lang={currentLang}
+                />
+            </Button>
+        </a>
+    </div>
 </div>
+
+{#if displayGame}
+    <div class="vignette" style={showGameVignette ? '' : 'display: none;'}>
+        <Error404 />
+        <Button on:click={hideGame}>
+            <LocalizedText
+                text="Go back"
+                key="redirect.leave"
+                lang={currentLang}
+            />
+        </Button>
+    </div>
+{/if}
 
 <style>
     * {
         font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;
     }
 
-    .game {
-        width: 400px;
-        height: 300px;
-        margin: 8px;
-        padding: 0;
-        border: 0;
-        position: relative;
-        background-image: url('/secret/sky.png');
-        overflow: hidden;
+    .text-container {
+        width: 50%;
     }
-    .game-penguin {
+    .image-container {
         position: absolute;
-        left: 40px;
+        right: 0;
         top: 0;
-        transform: scale(0.5);
-        transform-origin: left top;
+        width: 40%;
+        height: 100%;
+        padding-right: 24px;
+        overflow: hidden;
+        
+        display: flex;
+        flex-direction: column;
+        align-items: flex-end;
+        justify-content: center;
     }
-    .menu-penguin {
-        transform: scale(1.5);
-    }
-    .game-floor {
-        position: absolute;
+    :global(html[dir="rtl"]) .image-container {
+        padding-right: initial;
+        right: initial;
+        padding-left: 12px;
         left: 0;
-        bottom: -20px;
-        z-index: 9998;
+        align-items: flex-start;
     }
-    .game-bricks {
-        position: absolute;
-        z-index: 9997;
-    }
-    
-    .score-counter {
-        pointer-events: none;
-        font-size: 16px;
-        font-weight: bold;
-        font-style: italic;
-        color: black;
-        position: absolute;
-        margin-block: 0;
-        z-index: 9999;
-    }
-
-    .game-points {
-        font-size: 24px;
-        /* center */
-        left: 50%;
-        top: 10%;
-    }
-
-    .high-score {
-        right: 8px;
-        top: 8px;
-    }
-    
-    :global(html[dir="rtl"]) .game {
-        transform: scaleX(-1);
-        transform-origin: center;
-    }
-    :global(html[dir="rtl"]) .game-points {
-        transform: scaleX(-1);
-        transform-origin: center;
-    }
-
-    .hidden-button {
+    .image-button {
         padding: 0;
         margin: 0;
         border: 0;
         background: transparent;
-    }
 
-    .center-div {
+        width: 100%;
+        height: 60%;
+    }
+    .image-button > img {
+        width: 100%;
+        height: 100%;
+        object-fit: contain;
+    }
+    .vignette {
+        position: absolute;
+        left: 0;
+        top: 0;
+        width: 100%;
+        height: 100%;
+        background: rgba(0, 0, 0, 50%);
+        z-index: 999999;
+
         display: flex;
         flex-direction: column;
         align-items: center;
+        justify-content: center;
+    }
+
+    h1 {
+        font-size: 96px;
+        margin-block: 0;
+    }
+    a {
+        text-decoration: none;
+    }
+
+    .center-div {
+        position: absolute;
+        left: 0;
+        top: 3rem;
+        width: 100%;
+        height: calc(100% - 3rem);
+
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+    }
+    .bottom-left {
+        position: absolute;
+        left: 12px;
+        bottom: 12px;
+    }
+    :global(html[dir="rtl"]) .bottom-left {
+        left: initial;
+        right: 12px;
+    }
+
+    .card {
+        position: relative;
+        width: 70%;
+        height: 60%;
+        border-radius: 8px;
+        padding: 24px 16px;
+
+        border: 1px solid rgba(0, 0, 0, 35%);
+        overflow: auto;
+    }
+    :global(body.dark-mode) .card {
+        border-color: rgba(255, 255, 255, 35%);
     }
     :global(body.dark-mode) {
         color: white;
