b1426dcf459a52ad1fdd3059768110b6cfe3c7ce
JeremyGamer13 <69337718+JeremyGamer13@users.noreply.github.com>
2024-05-27 22:14:33 -0600

    update TOS and Privacy Policy

--- src/routes/terms/+page.svelte
+++ src/routes/terms/+page.svelte
@@ -42,7 +42,7 @@
     
         <main>
             <!-- UPDATE THIS WHEN MAKING CONTENT CHANGES -->
-            <p><i>Updated March 19th, 2024 (Mountain Time)</i></p>
+            <p><i>Updated May 27th, 2024 (Mountain Time)</i></p>
 
             <!-- some things are intentionally not formatted as they are in the HTML file on the website -->
             <p>
@@ -55,7 +55,9 @@
             </p>
             <p>
                 You may see websites such as <a href="https://scratch.mit.edu/">"Scratch"</a>,
-                <a href="https://turbowarp.org/">"TurboWarp"</a>, and <a href="https://auth.itinerary.eu.org/">"Scratch Auth"</a> referenced.
+                <a href="https://turbowarp.org/">"TurboWarp"</a>, <a href="https://oauth2.scratch-wiki.info/">"Scratch OAuth2"</a>,
+                and <a href="https://auth.itinerary.eu.org/">"Scratch Authentication"</a>
+                referenced in multiple places.
             </p>
             <p>
                 A "Service Administrator", "Admin", "Moderator", "Mod" or "Administrator" is a person who can either
@@ -84,14 +86,20 @@
                 at all times. 
             </p>
             <p>
-                PenguinMod is allowed to cut access to your ability to publish, update, or remix projects if you do not
-                abide by these rules, and PenguinMod has the right to cut your access to any services we provide if we believe you
+                PenguinMod is allowed to remove/limit access to your ability to publish, update, or remix projects if you do not
+                abide by these rules, and PenguinMod has the right to remove/limit your access to any services we provide if we believe you
                 are violating these guidelines or the Terms of Service.
             </p>
             <p>
-                PenguinMod is allowed to disable your ability to publish, update, remix or change any profile info you have if you do not
-                use language appropriate for kids or younger on your profile, and PenguinMod has the right to cut your access to any services
+                PenguinMod is allowed to remove/limit your ability to publish, update, remix or change any profile info you have if you do not
+                use language appropriate for kids or younger on your profile, and PenguinMod has the right to remove/limit your access to any services
                 we provide if we believe you are violating this ruling or the Terms of Service.
+            </p>
+            <p>
+                PenguinMod is allowed to remove/limit your ability to create or login to accounts if we believe you are violating the Terms of Service.
+                <br>
+                PenguinMod is allowed to limit or delete accounts created on PenguinMod if we believe you are violating the
+                <a href="https://penguinmod.com/guidelines/uploading">Uploading Guidelines</a> or Terms of Service.
             </p>
 
             <h2>Copyright</h2>
@@ -108,47 +116,25 @@
 
             <h2>Advertisements</h2>
             <p>
-                Advertisement Content: We may use Google AdSense to display advertisements on PenguinMod. These ads are provided by Google
+                We may use Google AdSense to display advertisements on PenguinMod. These ads are provided by Google
                 and are subject to Google's policies. PenguinMod is not responsible for the content of these ads.
             </p>
-            
-            <h2>Data Collection</h2>
-            <p>
-                <b>Information Collected:</b> We collect only the user's username, the content they upload, and their IP address.
-                <br>
-                <b>Purpose of Collection:</b>
-            </p>
-            <ul>
-                <li>
-                    <b>Username:</b> Usernames are collected for the sole purpose of displaying them to other users within the PenguinMod
-                    community.
-                </li>
-                <li>
-                    <b>IP Address:</b> IP addresses are collected for security reasons to validate user identity and ensure the integrity of the
-                    PenguinMod platform. Requests coming from the same IP too many times may lose connection to PenguinMod's online services.
-                </li>
-                <li>
-                    <b>Cookie Usage:</b> PenguinMod uses cookies to enhance user experience and store user progress and preferences on the website.
-                    Users can manage cookie preferences through their browser settings.
-                </li>
-            </ul>
-            
-            <h2>Third-Party Information Sharing</h2>
-            <p>PenguinMod does not share any user information, including usernames and IP addresses, with third parties.</p>
 
             <h2>Security</h2>
             <ul>
-                <li><b>Authentication System:</b> PenguinMod employs a secure authentication system ("Scratch Auth") to safeguard user login.</li>
+                <li><b>Authentication System:</b> PenguinMod allows users to use third-party websites to login or create accounts, such as
+                    <a href="https://oauth2.scratch-wiki.info/">"Scratch OAuth2"</a>,
+                    <a href="https://google.com/">"Google"</a>,
+                    or <a href="https://github.com/">"GitHub"</a>
+                    . PenguinMod also provides it's own secure authentication system for account creation and login.</li>
                 <li><b>Vulnerabilities:</b> PenguinMod has active developers and a small set of users looking through our code to ensure proper security.</li>
             </ul>
 
-            <h2>Limitation of Liability and Disclaimer</h2>
+            <h2>Exclusion of Damages</h2>
             <p>
-                No Assumption of Responsibility: PenguinMod, its owners, administrators, and affiliates do not assume responsibility for
+                PenguinMod, its owners, administrators, and affiliates do not assume responsibility for
                 any issues, damages, or consequences arising from the use of the website.
             </p>
-
-            <h2>Exclusion of Damages</h2>
             <p>
                 PenguinMod is not liable for any direct, indirect, incidental, consequential,
                 or special damages, including but not limited to loss of data, revenue, or profits.
@@ -172,7 +158,7 @@
                 encouraged to review this document periodically for any changes.
             </p><p>
                 PenguinMod will make effort to let users know of any changes to Uploading Guidelines, Terms of Service, or Privacy Policy through
-                the website's messaging system.
+                the website's messaging system and third-party services PenguinMod may have a precense on.
             </p><p>
                 By continuing to use PenguinMod, you agree to abide by the most recent version of these terms.
             </p>
