643c173666e145e4bb418fde2fc23499218091ff
Ianyourgod <104151459+Ianyourgod@users.noreply.github.com>
2024-11-28 21:32:31 -0600

    captcha

--- src/resources/authentication.js
+++ src/resources/authentication.js
@@ -27,7 +27,6 @@ class Authentication {
             username,
             password,
             captcha_token,
-            temp_use_cf: true,
         };
 
         return new Promise((resolve, reject) => {
