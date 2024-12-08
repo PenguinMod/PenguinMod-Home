2186aa22a5cc58acf2e994013214126cdad4eec4
jwklong <jwklong@gmx.com>
2024-12-08 21:35:10 +0000

    update env for idx

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -6,6 +6,8 @@
   env = {
     PUBLIC_API_URL="https://projects.penguinmod.com";
     PUBLIC_STUDIO_URL="https://studio.penguinmod.com";
+    PUBLIC_MAX_UPLOAD_SIZE="32";
+    PUBLIC_CAPTCHA_ENABLED="true";
   };
   idx.extensions = [
     "svelte.svelte-vscode"
