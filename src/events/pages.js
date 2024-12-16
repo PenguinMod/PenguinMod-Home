0bffd48d5a45a74ae5bd3c43f91b8d5a6ceec8bc
JeremyGamer13 <69337718+JeremyGamer13@users.noreply.github.com>
2024-12-15 18:42:42 -0700

    translate penguinjam & update site translations

--- src/events/pages.js
+++ src/events/pages.js
@@ -3,6 +3,10 @@ import ExampleEventEn from "./en/example.md?raw";
 import ExampleEventEs419 from "./es-419/example.md?raw";
 // PenguinJamWinter2024
 import PenguinJamWinter2024En from "./en/penguinjamwinter2024.md?raw";
+import PenguinJamWinter2024Es from "./es/penguinjamwinter2024.md?raw";
+import PenguinJamWinter2024Fr from "./fr/penguinjamwinter2024.md?raw";
+import PenguinJamWinter2024Tr from "./tr/penguinjamwinter2024.md?raw";
+import PenguinJamWinter2024Ru from "./ru/penguinjamwinter2024.md?raw";
 
 export default {
     "example": {
@@ -11,5 +15,9 @@ export default {
     },
     "penguinjamwinter2024": {
         "en": PenguinJamWinter2024En,
+        "es": PenguinJamWinter2024Es,
+        "fr": PenguinJamWinter2024Fr,
+        "tr": PenguinJamWinter2024Tr,
+        "ru": PenguinJamWinter2024Ru,
     },
 };
