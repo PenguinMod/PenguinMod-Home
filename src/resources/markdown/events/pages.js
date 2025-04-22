2e0699fc4cc5d4b42fcfd52c2b007663f95e745c
Tr1 <dgminhtrii@gmail.com>
2025-04-22 05:56:03 +0000

    mb

--- src/resources/markdown/events/pages.js
+++ src/resources/markdown/events/pages.js
@@ -15,7 +15,7 @@ import PenguinJamSpring2025Ja from "./ja/penguinjamspring2025.md?raw";
 import PenguinJamSpring2025Pl from "./pl/penguinjamspring2025.md?raw";
 import PenguinJamSpring2025Tr from "./tr/penguinjamspring2025.md?raw";
 import PenguinJamSpring2025Uk from "./uk/penguinjamspring2025.md?raw";
-import PenguinJamSpring2025Vi from "./vi/PenguinJamSpring2025.md?raw";
+import PenguinJamSpring2025Vi from "./vi/penguinJamSpring2025.md?raw";
 
 export default {
     "example": {
