1f9e7ce663e0e6e35a319716ccd05f3c4437560d
Ianyourgod <104151459+Ianyourgod@users.noreply.github.com>
2025-05-10 20:00:08 -0500

    fix that

--- src/routes/search/+page.svelte
+++ src/routes/search/+page.svelte
@@ -165,7 +165,7 @@
             <div style="height: 16px;" />
             <div class="more-projects-wrapper">
                 <Button
-                    label="<img alt='More' src='dropdown-caret-hd.png' width='20'></img>"
+                    label="<img alt='More' src='/dropdown-caret-hd.png' width='20'></img>"
                     on:click={() => {
                         page += 1;
                         fetchNewProjects();
