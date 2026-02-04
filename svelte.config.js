f9cba32f72c51d437f809121a26f8c489c05954c
JeremyGamer13 <69337718+JeremyGamer13@users.noreply.github.com>
2026-02-03 22:06:59 -0700

    try old static adapter with ext gallery workflow

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,16 @@
-import adapter from '@sveltejs/adapter-vercel';
+import adapter from '@sveltejs/adapter-static';
 
-/** @type {import('@sveltejs/kit').Config} */
-const config = {
-	kit: {
-		adapter: adapter({
-			runtime: 'nodejs22.x'
-		})
-	}
+export default {
+    kit: {
+        adapter: adapter({
+            // default options are shown. On some platforms
+            // these options are set automatically — see below
+            pages: 'public',
+            assets: 'public',
+            fallback: undefined,
+            precompress: false,
+            strict: true
+        })
+    },
+    //preprocess: [vitePreprocess()]
 };
-
-export default config;
