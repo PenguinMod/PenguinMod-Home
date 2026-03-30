205a747e45bcad8fe59935468fd8f368f0c5da4b
JeremyGamer13 <69337718+JeremyGamer13@users.noreply.github.com>
2026-03-29 22:04:32 -0600

    event is. Over

--- src/lib/Event/Component.svelte
+++ src/lib/Event/Component.svelte
@@ -3,7 +3,7 @@
     import Language from "../../resources/language.js";
     import EventPages from "../../resources/markdown/events/pages.js";
 
-    const isHavingEvent = true;
+    const isHavingEvent = false;
     const eventName = "Spring PenguinJam 2026";
     const eventPath = "penguinjamspring2026";
     
