83c8f3aabdece7749f4aaf567db4222455204d3a
JeremyGamer13 <69337718+JeremyGamer13@users.noreply.github.com>
2026-06-15 13:01:12 -0600

    No actiev 3vent

--- src/lib/Event/Component.svelte
+++ src/lib/Event/Component.svelte
@@ -3,7 +3,7 @@
     import Language from "../../resources/language.js";
     import EventPages from "../../resources/markdown/events/pages.js";
 
-    const isHavingEvent = true;
+    const isHavingEvent = false;
     const eventName = "All The Mods 2026";
     const eventPath = "allthemods2026";
     
